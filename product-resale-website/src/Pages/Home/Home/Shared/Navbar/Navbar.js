7afa8eae22c2f5f5c321458ad3992d2b69166271
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-24 16:51:28 +0600

    navbar

--- product-resale-website/src/Pages/Home/Home/Shared/Navbar/Navbar.js
+++ product-resale-website/src/Pages/Home/Home/Shared/Navbar/Navbar.js
@@ -1,9 +1,137 @@
-import React from 'react';
-
+import { Transition } from '@headlessui/react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../../../assets/logo/logo.png'
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div>
+      <nav className="bg-white text-black">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-14 w-14"
+                  src={logo}
+                  alt="Workflow"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className=" flex justify-center ml-96    space-x-4">
+                  <Link to='/'
+                   
+                    className="text-black  hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                  Home
+                  </Link>
+
+                  <Link
+                  to='/blog'
+                    className=" text-black  hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                 Blog
+                  </Link>
+
+                  <Link to='/login'
+                 
+                    className=" text-black  hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                  >
+            Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-blue-500 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <Link
+                to='/'
+                  className="hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+               Home
+                </Link>
+
+                <Link
+                 to='/blog'
+                  className="text-black hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+              Blog
+                </Link>
+
+                <Link 
+          to='/login'
+                  className="text-black hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+                Login
+                </Link>
+
             
+
+             
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+
         </div>
     );
 };
